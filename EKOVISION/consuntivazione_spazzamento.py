bd552d8dfa6ff82c921eb0a6f521bb259daad8a8
rmarzocchi84 <roberto.marzocchi@amiu.genova.it>
2026-06-23 13:49:05 +0200

    (il distinct è superfluo)

--- EKOVISION/consuntivazione_spazzamento.py
+++ EKOVISION/consuntivazione_spazzamento.py
@@ -458,10 +458,10 @@ and ve.id_causale <> %s'''
             join elem.percorsi p on p.id_percorso = ap.id_percorso 
             where ap.id_percorso = 
             (
-                select id_percorso_sit  from anagrafe_percorsi.date_percorsi_sit_uo ep 
+                select distinct id_percorso_sit  from anagrafe_percorsi.date_percorsi_sit_uo ep 
                 where id_percorso_sit is not null  
                 and cod_percorso = %s 
-                and data_inizio_validita <= %s 
+                and data_inizio_validita < %s 
                 and data_fine_validita >= %s
             ) and id_asta in (
                 select id_asta from elem.aste where id_via= %s
